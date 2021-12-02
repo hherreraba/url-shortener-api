ad9779298d7a2a80cd12985aee97625963ba1418
Ivan Herrera <hherrerabarragan@gmail.com>
2021-12-02 00:35:55 -0700

    Add update link url action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -23,6 +23,16 @@ class LinksController < ApplicationController
     end
   end
 
+  def update
+    link = Link.find_by(code: params[:id])
+
+    if link.update(original_url: params[:link][:original_url])
+      render json: { message: 'Url successfully updated' }
+    else
+      render json: { message: 'Something went wrong' }
+    end
+  end
+
   private
 
   def encode(last_id)
