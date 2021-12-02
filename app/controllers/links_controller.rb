a8894b909353fbe553079a016b42cdd14d9ca187
Ivan Herrera <hherrerabarragan@gmail.com>
2021-12-02 00:23:53 -0700

    Add create short url code action

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,4 +9,23 @@ class LinksController < ApplicationController
     links = Link.all.select('id', 'original_url', 'code')
     render json: { links: links }
   end
+
+  def create
+    last_id = Link.maximum(:id) || 1
+    code = encode(last_id.to_s)
+    link = Link.new(original_url: params[:link][:original_url],
+                    code: code, user_id: current_user.id)
+
+    if link.save
+      render json: { message: 'Short url sucessfully created' }
+    else
+      render json: { error: 'Something went wrong' }
+    end
+  end
+
+  private
+
+  def encode(last_id)
+    (Array.new(5) { [*'a'..'z', *'A'..'Z', *1..9].sample }.join + last_id).chars.shuffle.join
+  end
 end
